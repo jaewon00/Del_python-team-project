7b99f2ecd092992c8f15a680dd38f8c5271cfb9e
jaewon00 <hjaewon808@gmail.com>
2022-08-23 23:33:49 +0900

    -player 클래스 추가

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
-from game import Game
-
+from tkinter import Canvas
+from game import *
+from player import *
 game = Game()
 
-
-
 while game.running:
-    # g.playing = True
+    # g.playing = True 
+
     game.curr_menu.display_menu()
+
     game.game_loop()
+
+    
+
+ 
+    
